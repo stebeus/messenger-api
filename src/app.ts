b07e0d6347ad1af802340564f8f7efbedcb25a73
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-18 13:40:43 -0300

    feat: add `app.ts`

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+
+import { env } from './env.ts';
+import { auth } from './lib/auth.ts';
+import { HttpError, NotFoundError } from './utils/errors.ts';
+
+export const app = new Hono().basePath('/api');
+
+app.use(logger());
+
+app.all('/auth/*', (c) => auth.handler(c.req.raw));
+
+app.use(
+	cors({
+		origin: env.CLIENT_URL,
+		allowMethods: ['GET', 'POST', 'PATCH', 'DELETE'],
+		credentials: true,
+	}),
+);
+
+app.notFound((c) => {
+	const error = new NotFoundError();
+	return c.json({ error }, error.status);
+});
+
+app.onError((error, c) => {
+	const httpError = HttpError.isHttpError(error) ? error : new HttpError();
+	if (!HttpError.isHttpError(error)) console.error(error);
+	return c.json({ error: httpError }, httpError.status);
+});
