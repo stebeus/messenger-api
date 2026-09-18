93a1585dc76b2241d69ee011e93093891eb5656d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-18 12:33:22 -0300

    feat: add `HttpError` class

--- src/utils/errors.test.ts
+++ src/utils/errors.test.ts
@@ -1,6 +1,18 @@
 import { describe, expect, it } from 'vitest';
 
-import { catchError } from './errors.ts';
+import { catchError, HttpError } from './errors.ts';
+
+describe('HttpError.isHttpError', () => {
+	it('confirms that it is not an HTTP error', () => {
+		const error = new Error();
+		expect(HttpError.isHttpError(error)).toBeFalsy();
+	});
+
+	it('confirms that it is an HTTP error', () => {
+		const error = new HttpError();
+		expect(HttpError.isHttpError(error)).toBeTruthy();
+	});
+});
 
 describe('catchError', () => {
 	describe.for`
--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,3 +1,33 @@
+import type { ContentfulStatusCode } from 'hono/utils/http-status';
+
+import { STATUS_CODES } from 'node:http';
+
+import { HTTPException } from 'hono/http-exception';
+
+type HttpErrorOptions = Partial<{
+	res: Response;
+	message: string;
+	cause: unknown;
+}>;
+
+export class HttpError extends HTTPException {
+	static isHttpError(value: unknown) {
+		return value instanceof HTTPException;
+	}
+
+	readonly message;
+	readonly cause;
+
+	constructor(
+		status: ContentfulStatusCode = 500,
+		{ res, message = STATUS_CODES[status], cause }: HttpErrorOptions = {},
+	) {
+		super(status, { res, message, cause });
+		this.message = message ?? 'Internal Server Error';
+		this.cause = cause;
+	}
+}
+
 export const catchError = (value: unknown) => {
 	if (Error.isError(value)) return value;
 
