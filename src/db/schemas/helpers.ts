a65ab02e2b834ffb7e4f0301a378a69ef54c7737
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-18 13:49:31 -0300

    feat: add schema helpers

---
+++ src/db/schemas/helpers.ts
@@ -0,0 +1,26 @@
+import { type SQLWrapper, sql } from 'drizzle-orm';
+import * as p from 'drizzle-orm/pg-core';
+
+const mode = 'string';
+const withTimezone = true;
+
+export const id = p.bigint({ mode }).primaryKey().generatedAlwaysAsIdentity();
+
+export const createdAt = p.timestamp({ withTimezone }).defaultNow().notNull();
+
+export const updatedAt = p
+	.timestamp({ withTimezone })
+	.defaultNow()
+	.notNull()
+	.$onUpdate(() => new Date());
+
+export const timestamps = { createdAt, updatedAt } as const;
+
+export const base = { ...timestamps, id } as const;
+
+export const castToBigInt = (column: SQLWrapper) => sql`${column}::bigint`;
+
+export const reference = <PrimaryKey extends p.AnyPgColumn>(
+	primaryKey: () => PrimaryKey,
+	options?: p.ReferenceConfig['config'],
+) => p.bigint({ mode }).references(primaryKey, options);
