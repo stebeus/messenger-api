4ddae63709eb95cf63e15390eec3933f654986d0
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-18 15:41:39 -0300

    feat: add DB errors

---
+++ src/db/errors.ts
@@ -0,0 +1,43 @@
+import { toTitleCase } from '#utils/formatters.ts';
+
+type RepositoryInput = Record<string, unknown>;
+
+class RepositoryError extends Error {
+	static isRepositoryError(value: unknown) {
+		return value instanceof RepositoryError;
+	}
+
+	readonly operation;
+	readonly entity;
+	readonly input;
+
+	constructor(
+		operation: 'create' | 'find' | 'update' | 'delete',
+		entity: string,
+		input: RepositoryInput,
+	) {
+		super(`Failed to ${operation} entity`);
+
+		this.operation = operation;
+		this.entity = toTitleCase(entity);
+		this.input = input;
+	}
+}
+
+export class CreationError extends RepositoryError {
+	constructor(entity: string, input: RepositoryInput) {
+		super('create', entity, input);
+	}
+}
+
+export class UpdateError extends RepositoryError {
+	constructor(entity: string, input: RepositoryInput) {
+		super('update', entity, input);
+	}
+}
+
+export class DeletionError extends RepositoryError {
+	constructor(entity: string, input: RepositoryInput) {
+		super('delete', entity, input);
+	}
+}
