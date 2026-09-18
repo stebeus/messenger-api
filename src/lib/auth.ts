a5908e9b9bdc96dc40c36272f5492e0aa7ca8d06
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-18 13:38:13 -0300

    feat: add auth lib

---
+++ src/lib/auth.ts
@@ -0,0 +1,44 @@
+import { drizzleAdapter } from '@better-auth/drizzle-adapter/relations-v2';
+import { betterAuth } from 'better-auth/minimal';
+import { username } from 'better-auth/plugins';
+
+import { db } from '#db/client.ts';
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: 'pg',
+		schemaName: 'auth',
+		usePlural: true,
+	}),
+	advanced: {
+		database: {
+			generateId: 'serial',
+		},
+	},
+	emailAndPassword: {
+		autoSignIn: true,
+		enabled: true,
+	},
+	plugins: [
+		username({
+			schema: {
+				user: {
+					fields: {
+						displayUsername: 'displayName',
+					},
+				},
+			},
+		}),
+	],
+	user: {
+		additionalFields: {
+			bio: {
+				required: false,
+				type: 'string',
+			},
+		},
+		fields: {
+			image: 'avatar',
+		},
+	},
+});
