a40590a8fc07948c898b39050d1ee1dc7b9c7f50
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-19 14:44:26 -0300

    feat: add member types

---
+++ src/features/members/types.ts
@@ -0,0 +1,27 @@
+import type { BodyDto, Id } from '#contracts/index.ts';
+import type { Selection } from '#db/types.ts';
+import type { ConversationParams } from '#features/conversations/contracts/dtos.ts';
+import type { GroupParams } from '#features/conversations/groups/contracts/dtos.ts';
+import type { UserParams } from '#features/users/contracts/dtos.ts';
+import type { UsersSelection } from '#features/users/types.ts';
+import type { Member, UpdateMemberBody } from './contracts/index.ts';
+
+export type MembersSelection = ConversationParams & UsersSelection;
+
+export type MemberSelection = Selection<Member>;
+
+export type MemberArgs = Pick<Member, 'userId' | 'conversationId'>;
+
+export type GroupMember = UserParams & GroupParams;
+
+export type ListMemberArgs = GroupMember & UsersSelection;
+
+export type Management = GroupParams & {
+	actorId: Id;
+};
+
+export type MemberManagement = Management & {
+	targetId: Id;
+};
+
+export type RoleManagement = MemberManagement & BodyDto<UpdateMemberBody>;
