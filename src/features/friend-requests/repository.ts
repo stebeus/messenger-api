2b8c9c19b6f53701e7d1d8314fbb6bb7454d15f7
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-19 15:45:19 -0300

    feat: add friend request repo

---
+++ src/features/friend-requests/repository.ts
@@ -0,0 +1,58 @@
+import type { UserPair } from '#features/users/contracts/entity.ts';
+import type { NewFriendRequest } from './contracts/entity.ts';
+import type { FriendRequestArgs, ListFriendRequestArgs } from './types.ts';
+
+import { CreationError, type DatabaseContext, DeletionError, db, orderBy } from '#db/index.ts';
+import { friendRequests } from '#db/schemas/social.ts';
+import { containsName, createUserFilter } from '#features/users/helpers.ts';
+
+import { isFriendRequest } from './helpers.ts';
+
+const create = async ({ tx = db, ...values }: DatabaseContext<NewFriendRequest>) => {
+	const [data] = await tx.insert(friendRequests).values(values).returning();
+	if (data == null) throw new CreationError('friend request', values);
+	return data;
+};
+const find = async ({
+	userId,
+	query: { q, direction, sort, order },
+	tx = db,
+}: DatabaseContext<ListFriendRequestArgs>) => {
+	const displayName = containsName(q);
+	const userFilter = createUserFilter(userId);
+
+	const directionFilter = {
+		incoming: { requesterId: userId, recipient: displayName },
+		outgoing: { recipientId: userId, requester: displayName },
+	} as const;
+
+	const where =
+		direction == null
+			? { OR: [directionFilter.incoming, directionFilter.outgoing] }
+			: directionFilter[direction];
+
+	return await tx.query.friendRequests.findMany({
+		where,
+		with: { requester: userFilter, recipient: userFilter },
+		...orderBy(sort, order),
+	});
+};
+
+const findOne = async ({ user1Id, user2Id, tx = db }: DatabaseContext<UserPair>) =>
+	await tx.query.friendRequests.findFirst({
+		where: {
+			OR: [
+				{ requesterId: user1Id, recipientId: user2Id },
+				{ requesterId: user2Id, recipientId: user1Id },
+			],
+		},
+		with: { requester: true, recipient: true },
+	});
+
+const destroy = async ({ tx = db, ...values }: DatabaseContext<FriendRequestArgs>) => {
+	const [data] = await tx.delete(friendRequests).where(isFriendRequest(values)).returning();
+	if (data == null) throw new DeletionError('friend request', values);
+	return data;
+};
+
+export const friendRequestRepository = { create, find, findOne, destroy } as const;
