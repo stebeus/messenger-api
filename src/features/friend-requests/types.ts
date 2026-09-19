36378b84cd1372f920ebee3da988e7645578f509
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-19 15:45:13 -0300

    feat: add `ListFriendRequestArgs` type

--- src/features/friend-requests/types.ts
+++ src/features/friend-requests/types.ts
@@ -1,6 +1,10 @@
+import type { QueryDto } from '#contracts/dtos.ts';
 import type { Selection } from '#db/types.ts';
-import type { FriendRequest } from './contracts/entity.ts';
+import type { UserParams } from '#features/users/contracts/dtos.ts';
+import type { FriendRequest, FriendRequestQuery } from './contracts/index.ts';
 
 export type FriendRequestSelection = Selection<FriendRequest>;
 
 export type FriendRequestArgs = Pick<FriendRequest, 'requesterId' | 'recipientId'>;
+
+export type ListFriendRequestArgs = UserParams & QueryDto<FriendRequestQuery>;
