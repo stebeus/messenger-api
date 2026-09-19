7280c57c1e86dbd5af07cd3a396a1aed28f7093a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-19 08:04:18 -0300

    feat: add `formatMinLength` util

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,6 +1,21 @@
+const sanitizeLength = (length: number) => {
+	if (!Number.isInteger(length)) throw new Error('Length must be an integer');
+	if (length < 1) throw new Error('Length must be positive');
+	return length;
+};
+
 export const capitalize = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 export const toTitleCase = (string: string) => {
 	const initialsRegex = /\b\w/g;
 	return string.toLowerCase().replace(initialsRegex, capitalize);
+};
+
+export const formatMinLength = (fieldName: string, length = 1) => {
+	const sanitizedLength = sanitizeLength(length);
+
+	const message =
+		sanitizedLength === 1 ? 'is required' : `must be at least ${sanitizedLength} characters long`;
+
+	return `${capitalize(fieldName)} ${message}`;
 };
--- src/utils/tests/formatters.test.ts
+++ src/utils/tests/formatters.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { capitalize, toTitleCase } from '#utils/formatters.ts';
+import { capitalize, formatMinLength, toTitleCase } from '#utils/formatters.ts';
 
 describe('capitalize', () => {
 	it('capitalizes strings', () => {
@@ -13,5 +13,27 @@ describe('toTitleCase', () => {
 	it('title cases strings', () => {
 		const titleCased = toTitleCase('hello, world!');
 		expect(titleCased).toBe('Hello, World!');
+	});
+});
+
+describe('formatMinLength', () => {
+	describe('Given invalid lengths', () => {
+		it('rejects floats', () => {
+			expect(() => formatMinLength('field', 0.1)).toThrow('Length must be an integer');
+		});
+
+		it('rejects negative integers', () => {
+			expect(() => formatMinLength('field', -1)).toThrow('Length must be positive');
+		});
+	});
+
+	it('defaults to a required field message', () => {
+		const formatted = formatMinLength('field');
+		expect(formatted).toBe('Field is required');
+	});
+
+	it('formats minimum length messages', () => {
+		const formatted = formatMinLength('field', 2);
+		expect(formatted).toBe('Field must be at least 2 characters long');
 	});
 });
