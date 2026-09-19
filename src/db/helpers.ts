bde81463135e5d7abdd301dad418c9648dc8a6e8
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-19 09:15:47 -0300

    feat: add DB helpers

---
+++ src/db/helpers.ts
@@ -0,0 +1,13 @@
+import type { Id } from '#contracts/entities.ts';
+
+export const contains = (query?: string) =>
+	query == null ? undefined : ({ like: `%${query}%` } as const);
+
+export const orderBy = (sort = 'createdAt', order = 'asc') =>
+	({ orderBy: { [sort]: order } }) as const;
+
+export const parseId = (id: Id) => {
+	const parsedId = Number.parseInt(id, 10);
+	if (!Number.isInteger(parsedId)) throw new Error('Parsed ID must be an integer');
+	return parsedId;
+};
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,4 @@
 export * from './client.ts';
 export * from './errors.ts';
+export * from './helpers.ts';
 export * from './types.ts';
