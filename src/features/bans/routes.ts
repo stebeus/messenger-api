afc961e1869a38343df5b6067f45caba76250657
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-20 16:30:03 -0300

    feat: add ban routes

---
+++ src/features/bans/routes.ts
@@ -0,0 +1,77 @@
+import { Hono } from 'hono';
+
+import { GroupMemberParams, GroupParams } from '#features/conversations/groups/contracts/dtos.ts';
+import { UserQuery } from '#features/users/contracts/dtos.ts';
+import { requireAuth, validate } from '#middleware/index.ts';
+
+import { CreateBanBody, UpdateBanBody } from './contracts/dtos.ts';
+import { banService } from './services.ts';
+
+export const bans = new Hono();
+
+bans.get(
+	'/',
+	validate('param', GroupParams),
+	validate('query', UserQuery),
+	requireAuth,
+	async (c) => {
+		const { groupId } = c.req.valid('param');
+		const { user } = c.var.auth;
+		const query = c.req.valid('query');
+
+		const data = await banService.find({ groupId, userId: user.id, query });
+
+		return c.json({ data });
+	},
+);
+
+bans.post(
+	'/:memberId',
+	validate('param', GroupMemberParams),
+	validate('json', CreateBanBody),
+	requireAuth,
+	async (c) => {
+		const { user } = c.var.auth;
+		const { groupId, memberId } = c.req.valid('param');
+		const body = c.req.valid('json');
+
+		const data = await banService.create({
+			...body,
+			actorId: user.id,
+			targetId: memberId,
+			groupId,
+		});
+
+		return c.json({ data }, 201);
+	},
+);
+
+bans.patch(
+	'/:memberId',
+	validate('param', GroupMemberParams),
+	validate('json', UpdateBanBody),
+	requireAuth,
+	async (c) => {
+		const { user } = c.var.auth;
+		const { groupId, memberId } = c.req.valid('param');
+		const body = c.req.valid('json');
+
+		const data = await banService.update({
+			...body,
+			actorId: user.id,
+			targetId: memberId,
+			groupId,
+		});
+
+		return c.json({ data });
+	},
+);
+
+bans.delete('/:memberId', validate('param', GroupMemberParams), requireAuth, async (c) => {
+	const { user } = c.var.auth;
+	const { groupId, memberId } = c.req.valid('param');
+
+	const data = await banService.destroy({ actorId: user.id, targetId: memberId, groupId });
+
+	return c.json({ data });
+});
