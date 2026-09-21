f7f80f65061839903ff95eecde40b43f17e4d972
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-21 10:49:20 -0300

    feat: add `BanQuery` DTO

--- src/features/bans/contracts/dtos.ts
+++ src/features/bans/contracts/dtos.ts
@@ -1,10 +1,23 @@
-import type * as z from 'zod';
+import * as z from 'zod';
+
+import { UserQuery, userSorts } from '#features/users/contracts/dtos.ts';
 
 import { BanUpdate, NewBan } from './entity.ts';
+
+export const banSorts = [...userSorts, 'expiresAt'] as const;
+
+export const BanQuery = z
+	.object({
+		...UserQuery.shape,
+		sort: z.enum(banSorts).default('createdAt'),
+	})
+	.partial();
 
 export const CreateBanBody = NewBan.omit({ userId: true, groupId: true });
 
 export const UpdateBanBody = BanUpdate.omit({ userId: true, groupId: true });
+
+export type BanQuery = z.infer<typeof BanQuery>;
 
 export type CreateBanBody = z.infer<typeof CreateBanBody>;
 
--- src/features/bans/types.ts
+++ src/features/bans/types.ts
@@ -1,15 +1,17 @@
+import type { QueryDto } from '#contracts/dtos.ts';
 import type { Selection } from '#db/types.ts';
 import type { GroupParams } from '#features/conversations/groups/contracts/dtos.ts';
 import type { GroupMember, MemberManagement } from '#features/members/types.ts';
-import type { UsersSelection } from '#features/users/types.ts';
-import type { Ban, CreateBanBody, UpdateBanBody } from './contracts/index.ts';
+import type { Ban, BanQuery, CreateBanBody, UpdateBanBody } from './contracts/index.ts';
 
-export type BansSelection = GroupParams & UsersSelection;
+type BanQueryDto = QueryDto<BanQuery>;
+
+export type BansSelection = GroupParams & BanQueryDto;
 
 export type BanSelection = Selection<Ban>;
 
 export type CreateBanArgs = MemberManagement & CreateBanBody;
 
-export type ListBanArgs = GroupMember & UsersSelection;
+export type ListBanArgs = GroupMember & BanQueryDto;
 
 export type UpdateBanArgs = MemberManagement & UpdateBanBody;
