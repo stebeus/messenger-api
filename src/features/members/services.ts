c49d1cf3313567c87a60c87eb23a009ba2a4dded
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-21 08:40:53 -0300

    fix: include member creation in the transaction

--- src/features/members/services.ts
+++ src/features/members/services.ts
@@ -18,7 +18,7 @@ import { memberRepository } from './repository.ts';
 const create = async ({ userId, conversationId, role, tx }: DatabaseContext<NewMember>) => {
 	const member = await memberRepository.findOne({ userId, conversationId, tx });
 	if (member != null) throw new ConflictError({ message: 'Member already exists' });
-	return await memberRepository.create({ userId, conversationId, role });
+	return await memberRepository.create({ userId, conversationId, role, tx });
 };
 
 const joinGroup = async ({ userId, groupId }: GroupMember) => {
