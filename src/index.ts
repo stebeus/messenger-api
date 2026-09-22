623ac2c41826607d82f7c4370a39c1ba1bba16d0
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-22 14:36:30 -0300

    feat: set up WebSocket server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { serve } from '@hono/node-server';
+import { WebSocketServer } from 'ws';
 
 import { app } from './app.ts';
 import { config } from './config/index.ts';
 
-serve({ fetch: app.fetch, port: config.port }, ({ port }) =>
+const wss = new WebSocketServer({ noServer: true });
+
+serve({ fetch: app.fetch, port: config.port, websocket: { server: wss } }, ({ port }) =>
 	console.log(`Server running on port ${port}`),
 );
