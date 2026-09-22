e49a244f9b0543890207033280ee5c4f78df0ace
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-22 11:30:54 -0300

    refactor: specify param file size unit

--- src/middleware/body-limit.ts
+++ src/middleware/body-limit.ts
@@ -2,9 +2,9 @@ import { bodyLimit } from 'hono/body-limit';
 
 import { ContentTooLargeError } from '#utils/errors.ts';
 
-export const limitBody = (maxSize = 512) =>
+export const limitBody = (maxSizeInKbs = 512) =>
 	bodyLimit({
-		maxSize: maxSize * 1024,
+		maxSize: maxSizeInKbs * 1024,
 		onError: () => {
 			throw new ContentTooLargeError({ message: 'File is too large' });
 		},
