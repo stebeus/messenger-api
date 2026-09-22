9b54fdc9662d272dc413dfc8daf31c7c6511719b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-22 09:46:53 -0300

    feat: add upload wrappers

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,8 +1,30 @@
-import { StorageClient } from '@supabase/storage-js';
+import type { ContentfulStatusCode } from 'hono/utils/http-status';
+
+import { type FileOptions, StorageClient } from '@supabase/storage-js';
 
 import { env } from '#env.ts';
+import { HttpError } from '#utils/errors.ts';
+
+type Bucket = 'avatar';
 
 export const storage = new StorageClient(`${env.SUPABASE_URL}/storage/v1`, {
 	Authorization: `Bearer ${env.SUPABASE_PUBLISHABLE_KEY}`,
 	apikey: env.SUPABASE_PUBLISHABLE_KEY,
 });
+
+export const upload = async (bucket: Bucket, path: string, file: File, options?: FileOptions) => {
+	const { data, error } = await storage.from(bucket).upload(path, file, options);
+
+	if (error != null) {
+		throw new HttpError(error.status as ContentfulStatusCode, { message: error.message });
+	}
+
+	return data;
+};
+
+export const maybeUpload = async (
+	bucket: Bucket,
+	path: string,
+	file?: File | null,
+	options?: FileOptions,
+) => (file == null ? undefined : await upload(bucket, path, file, options));
