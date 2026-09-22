7ba7b6cd6866835d3df811621a2d07a05b386f9a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-22 11:14:58 -0300

    fix: create unique file path

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -7,13 +7,19 @@ import { HttpError } from '#utils/errors.ts';
 
 type Bucket = 'avatars';
 
+const createFilePath = (path: string, file: File) => {
+	const fileExtension = file.name.split('.').pop();
+	return `${path}/${crypto.randomUUID()}.${fileExtension}`;
+};
+
 export const storage = new StorageClient(config.storage.url, {
 	Authorization: `Bearer ${config.storage.secret}`,
 	apikey: config.storage.secret,
 });
 
 export const upload = async (bucket: Bucket, path: string, file: File, options?: FileOptions) => {
-	const { data, error } = await storage.from(bucket).upload(path, file, options);
+	const filePath = createFilePath(path, file);
+	const { data, error } = await storage.from(bucket).upload(filePath, file, options);
 
 	if (error != null) {
 		throw new HttpError(error.status as ContentfulStatusCode, { message: error.message });
