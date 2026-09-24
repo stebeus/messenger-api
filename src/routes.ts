634b209388a3390d9f19ac11e468b4899345a524
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-24 12:27:14 -0300

    style: use relative route import

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,9 @@
 import { Hono } from 'hono';
 
-import { conversations } from '#features/conversations/routes.ts';
-
 import { dms } from './features/conversations/dms/routes.ts';
 import { groups } from './features/conversations/groups/routes.ts';
 import { messages } from './features/conversations/messages/routes.ts';
+import { conversations } from './features/conversations/routes.ts';
 import { friendRequests } from './features/friend-requests/routes.ts';
 import { friends } from './features/friendships/routes.ts';
 import { users } from './features/users/routes.ts';
