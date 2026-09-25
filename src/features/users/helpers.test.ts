1512934691f45721849b6742502e9ac8950c1f93
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-25 14:06:44 -0300

    test: test user helpers

---
+++ src/features/users/helpers.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest';
+
+import { containsName } from './helpers.ts';
+
+describe('containsName', () => {
+	it('creates nothing when given no name', () => {
+		const userQuery = containsName();
+		expect(userQuery).toBeUndefined();
+	});
+
+	it('creates a search query when given a name', () => {
+		const userQuery = containsName('John Doe');
+
+		expect(userQuery).toStrictEqual({
+			OR: [{ username: { like: '%John Doe%' } }, { displayName: { like: '%John Doe%' } }],
+		});
+	});
+});
