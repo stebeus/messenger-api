20506c359e5beb2770885594e8fc418c22b96728
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-25 08:58:39 -0300

    refactor: rename the auth env type

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,13 +3,13 @@ import { createMiddleware } from 'hono/factory';
 import { auth } from '#lib/auth.ts';
 import { UnauthorizedError } from '#utils/errors.ts';
 
-type Env = {
+type AuthEnv = {
 	Variables: {
 		auth: typeof auth.$Infer.Session;
 	};
 };
 
-export const requireAuth = createMiddleware<Env>(async (c, next) => {
+export const requireAuth = createMiddleware<AuthEnv>(async (c, next) => {
 	const session = await auth.api.getSession({ headers: c.req.raw.headers });
 	if (session == null) throw new UnauthorizedError();
 
